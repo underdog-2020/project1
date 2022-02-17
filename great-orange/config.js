32061958674399e3d6c8d745c0bf6180ab805d83
underdog-2020 <77315190+underdog-2020@users.noreply.github.com>
2022-02-17 17:14:02 +1100

    Add files via upload

---
+++ great-orange/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDOugn3gSmzh5dbXLHCX6aHL0bq8Jng9A0",
+  authDomain: "ttt5-ce19b.firebaseapp.com",
+  projectId: "ttt5-ce19b",
+  storageBucket: "ttt5-ce19b.appspot.com",
+  messagingSenderId: "1037910754284",
+  appId: "1:1037910754284:web:6c4690775bc9158ee876c9"
+};
